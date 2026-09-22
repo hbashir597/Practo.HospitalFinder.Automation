f43cec6dafeddf7c6da4856e9d46a2070496a887
hbashir597 <hbashir597@gmail.com>
2026-09-22 13:27:59 +0100

    chore: add Selenium BDD test project

---
+++ Practo.HospitalFinder.SeleniumBDD/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Practo.HospitalFinder.SeleniumBDD;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
