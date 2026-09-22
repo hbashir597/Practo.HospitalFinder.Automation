1b8bfb433a5b4b9640cc5b09e4689f8ea71930e9
hbashir597 <hbashir597@gmail.com>
2026-09-22 10:49:12 +0100

    test: configure Playwright and add smoke test

---
+++ Practo.HospitalFinder.Playwright/PlaywrightSmokeTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using System.Text.RegularExpressions;
+
+namespace Practo.HospitalFinder.Playwright;
+
+[TestFixture]
+public class PlaywrightSmokeTests : PageTest
+{
+    [Test]
+    public async Task PlaywrightCanOpenPractoHomepage()
+    {
+        await Page.GotoAsync("https://www.practo.com/");
+
+        await Expect(Page).ToHaveTitleAsync(
+            new Regex("Practo", RegexOptions.IgnoreCase));
+    }
+}
