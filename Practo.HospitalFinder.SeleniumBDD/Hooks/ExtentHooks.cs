27e2241cb47996a0bb473a931ed31881ad3a785b
hbashir597 <hbashir597@gmail.com>
2026-09-24 14:09:54 +0100

    test: add reporting and failure evidence

---
+++ Practo.HospitalFinder.SeleniumBDD/Hooks/ExtentHooks.cs
@@ -0,0 +1,58 @@
+﻿using AventStack.ExtentReports;
+using Practo.HospitalFinder.SeleniumBDD.Reporting;
+using Reqnroll;
+
+namespace Practo.HospitalFinder.SeleniumBDD.Hooks;
+
+[Binding]
+public sealed class ExtentHooks
+{
+    private readonly ScenarioContext _scenarioContext;
+    private ExtentTest? _test;
+
+    public ExtentHooks(
+        ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+
+    [BeforeScenario(Order = -50)]
+    public void BeforeScenario()
+    {
+        _test =
+            ExtentReport.Instance.CreateTest(
+                _scenarioContext.ScenarioInfo.Title);
+
+        foreach (var tag in
+                 _scenarioContext.ScenarioInfo.CombinedTags)
+        {
+            _test.AssignCategory(tag);
+        }
+    }
+
+    [AfterStep]
+    public void AfterStep()
+    {
+        var stepText =
+            _scenarioContext.StepContext
+                .StepInfo.Text;
+
+        _test?.Info(stepText);
+    }
+
+    [AfterScenario(Order = 200)]
+    public void AfterScenario()
+    {
+        if (_scenarioContext.TestError is null)
+        {
+            _test?.Pass("Scenario passed");
+        }
+        else
+        {
+            _test?.Fail(
+                _scenarioContext.TestError);
+        }
+
+        ExtentReport.Instance.Flush();
+    }
+}
---
+++ Practo.HospitalFinder.SeleniumBDD/Reporting/ExtentReport.cs
@@ -0,0 +1,50 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+
+namespace Practo.HospitalFinder.SeleniumBDD.Reporting;
+
+public static class ExtentReport
+{
+    public static readonly ExtentReports Instance;
+
+    static ExtentReport()
+    {
+        var projectDirectory =
+            Path.GetFullPath(
+                Path.Combine(
+                    AppContext.BaseDirectory,
+                    "..",
+                    "..",
+                    ".."));
+
+        var reportDirectory =
+            Path.Combine(
+                projectDirectory,
+                "TestResults");
+
+        Directory.CreateDirectory(reportDirectory);
+
+        var reportPath =
+            Path.Combine(
+                reportDirectory,
+                "ExtentReport.html");
+
+        var sparkReporter =
+            new ExtentSparkReporter(reportPath);
+
+        sparkReporter.Config.DocumentTitle =
+            "Practo Hospital Finder Automation";
+
+        Instance = new ExtentReports();
+
+        Instance.AttachReporter(sparkReporter);
+
+        Instance.AddSystemInfo(
+            "Framework",
+            "Reqnroll + NUnit + Selenium");
+
+        Instance.AddSystemInfo(
+            "Application",
+            "Practo Hospital Finder");
+    }
+}
